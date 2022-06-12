6f8fec54d1c9b54ba9b521556aff45e5e1f79106
Saummya Singh <46069982+Saummya@users.noreply.github.com>
2022-06-12 23:44:25 +0530

    Create Count pairs with given sum.py

---
+++ LOVE-BABBAR DSA QUES/ARRAY/Count pairs with given sum.py
@@ -0,0 +1,66 @@
+'''
+Given an array of N integers, and an integer K, find the number of pairs of elements in the array whose sum is equal to K.
+
+
+Example 1:
+
+Input:
+N = 4, K = 6
+arr[] = {1, 5, 7, 1}
+Output: 2
+Explanation: 
+arr[0] + arr[1] = 1 + 5 = 6 
+and arr[1] + arr[3] = 5 + 1 = 6.
+
+Example 2:
+
+Input:
+N = 4, K = 2
+arr[] = {1, 1, 1, 1}
+Output: 6
+Explanation: 
+Each 1 will produce sum 2 with any 1.
+
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function getPairsCount() which takes arr[], n and k as input parameters and returns the number of pairs that have sum K.
+
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(N)
+'''
+
+#User function Template for python3
+
+class Solution:
+    def getPairsCount(self, arr, n, k):
+        # code here
+        m = [0] * 2000000
+  
+        for i in range(0, n):
+            m[arr[i]] += 1
+            
+        twice_count = 0
+    
+        for i in range(0, n):
+            twice_count += m[k - arr[i]]
+            if (k - arr[i] == arr[i]):
+                twice_count -= 1
+     
+
+        return int(twice_count / 2)
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        n, k = list(map(int, input().strip().split()))
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        ans = ob.getPairsCount(arr, n, k)
+        print(ans)
+        tc -= 1
+
+# } Driver Code Ends
