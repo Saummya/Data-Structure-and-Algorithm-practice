a6e05a7c7d4facb9393afaca53602e5df6dab58c
Saummya Singh <46069982+Saummya@users.noreply.github.com>
2022-06-17 22:18:07 +0530

    Create Row with max 1s.py

---
+++ LOVE-BABBAR DSA QUES/MATRIX/Row with max 1s.py
@@ -0,0 +1,78 @@
+'''
+Given a boolean 2D array of n x m dimensions where each row is sorted. Find the 0-based index of the first row that has the maximum number of 1's.
+
+Example 1:
+
+Input: 
+N = 4 , M = 4
+Arr[][] = {{0, 1, 1, 1},
+           {0, 0, 1, 1},
+           {1, 1, 1, 1},
+           {0, 0, 0, 0}}
+Output: 2
+Explanation: Row 2 contains 4 1's (0-based
+indexing).
+
+Example 2:
+
+Input: 
+N = 2, M = 2
+Arr[][] = {{0, 0}, {1, 1}}
+Output: 1
+Explanation: Row 1 contains 2 1's (0-based
+indexing).
+
+Your Task:  
+You don't need to read input or print anything. Your task is to complete the function rowWithMax1s() which takes the array of booleans arr[][], n and m as input parameters and returns the 0-based index of the first row that has the most number of 1s. If no such row exists, return -1.
+ 
+
+Expected Time Complexity: O(N+M)
+Expected Auxiliary Space: O(1)
+
+
+Constraints:
+1 ≤ N, M ≤ 103
+0 ≤ Arr[i][j] ≤ 1 
+'''
+
+#User function Template for python3
+class Solution:
+
+	def rowWithMax1s(self,arr, n, m):
+		# code here
+		count = arr[0].count(1)
+		
+		for item in arr :
+		    count = max(count, item.count(1))
+		if count == 0:
+            return -1
+		i = 0
+		for item in arr :
+		    if item.count(1) == count :
+		        break
+		    i += 1
+		    
+		return i
+
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        n, m = list(map(int, input().strip().split()))
+        
+        inputLine = list(map(int, input().strip().split()))
+        arr = [[0 for j in range(m)] for i in range(n)]
+        
+        for i in range(n):
+            for j in range(m):
+                arr[i][j] = inputLine[i * m + j]
+        ob = Solution()
+        ans = ob.rowWithMax1s(arr, n, m)
+        print(ans)
+        tc -= 1
+
+# } Driver Code Ends
